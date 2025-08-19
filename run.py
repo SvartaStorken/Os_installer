b0a8311870889c7082a6ae00ded5c2babffa9c69
Martin <martin_junkan@hotmail.com>
2025-08-19 12:20:36 +0200

    add chose

--- run.py
+++ run.py
@@ -1,1 +1,22 @@
 print ("Welcome to os_installer")
+
+print ("***********************")
+
+print ("What do you like to do?")
+
+print ("***********************")
+
+print ("New install 1")
+
+print ("***********************")
+
+print ("Edit installation Template 2")
+
+print ("***********************")
+
+print ("Please type your choice")
+chose = input()
+
+print ("***********************")
+
+print ('You chose ' + chose + ' is that correct? yes/no')
