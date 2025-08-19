4db1549f8c57bd31362a9466a8254c2d90ee6661
Martin <martin_junkan@hotmail.com>
2025-08-19 13:24:13 +0200

    AI corrections and y for continue

--- run.py
+++ run.py
@@ -1,34 +1,30 @@
 correct_answer = False
 
-print ("Welcome to os_installer")
+print("Welcome to os_installer")
+print("***********************")
+print("What do you like to do?")
+print("***********************")
 
-print ("***********************")
-
-print ("What do you like to do?")
-
-print ("***********************")
-
+# while not loop to ensure correct answer
 while not correct_answer:
-
-    print ("New install 1")
-
-    print ("***********************")
-
-    print ("Edit installation Template 2")
-
-    print ("***********************")
-
-    print ("Please type your choice")
+    print("New install                  [1]")
+    print("Edit installation Template   [2]")
+    print("***********************")
+    print("Please type your choice:")
+    
     chose = input()
 
-    print ("***********************")
-
-    print ('You chose ' + chose + ' is that correct? yes/no')
-
+    print("***********************")
+    print(f'You chose "{chose}". Is that correct? (yes/no)')
+    
     user_input = input()
-    if user_input == "yes":
+
+    # anny answer starts with y is correct
+    if user_input.lower().startswith('y'):
         correct_answer = True
+    else:
+        print(f'Your chose "{chose}" was wrong. Try again.')
+        print("***********************\n")
 
-print ("***********************")
-
-print ("continuing process")
+print("***********************")
+print("Continuing process...")
